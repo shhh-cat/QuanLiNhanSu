f0a9e93cc22cccbf604334b58e27f729b944a704
Nguyễn Vân Thiên <main.laotienboi2804@gmail.com>
2021-08-27 21:08:23 +0700

    add sql

---
+++ jdbc_humanresource.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th8 27, 2021 lúc 04:07 PM
+-- Phiên bản máy phục vụ: 10.4.20-MariaDB
+-- Phiên bản PHP: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `jdbc_humanresource`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `department`
+--
+
+CREATE TABLE `department` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `min_salary` int(10) NOT NULL,
+  `max_salary` int(10) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `department`
+--
+
+INSERT INTO `department` (`id`, `name`, `min_salary`, `max_salary`, `created_at`, `updated_at`) VALUES
+(1, 'Kế toán', 10000000, 15000000, '2021-08-27 09:50:54', '2021-08-27 09:50:54'),
+(2, 'Marketing', 8000000, 15000000, '2021-08-27 09:51:20', '2021-08-27 09:51:20'),
+(3, 'IT', 10000000, 20000000, '2021-08-27 09:51:37', '2021-08-27 09:51:37'),
+(4, 'Nhân sự', 12000000, 18000000, '2021-08-27 09:51:55', '2021-08-27 09:51:55'),
+(5, 'Sales', 5000000, 20000000, '2021-08-27 09:53:58', '2021-08-27 09:53:58');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` bigint(20) NOT NULL,
+  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phonenumber` varchar(20) NOT NULL,
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `idnumber` varchar(20) NOT NULL,
+  `department_id` bigint(20) NOT NULL,
+  `position` varchar(50) DEFAULT NULL,
+  `bonus_position` int(3) DEFAULT NULL,
+  `salary` int(10) NOT NULL,
+  `project_id` bigint(20) DEFAULT NULL,
+  `start_date` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `employee`
+--
+
+INSERT INTO `employee` (`id`, `firstname`, `lastname`, `phonenumber`, `address`, `gender`, `idnumber`, `department_id`, `position`, `bonus_position`, `salary`, `project_id`, `start_date`, `created_at`, `updated_at`) VALUES
+(1, 'A', 'Nguyen Thi', '098756412', 'New York', 1, '1223432321', 1, 'Nhân viên', 0, 10000000, 1, '2021-05-04', '2021-08-27 09:55:20', '2021-08-27 09:55:20'),
+(2, 'B', 'Nguyen Van', '0123123515', 'Tp.HCM', 1, '21987654312', 2, 'Phó phòng', 5, 8000000, 2, '2020-10-08', '2021-08-27 09:57:06', '2021-08-27 09:57:06'),
+(3, 'C', 'Tran', '0244363563', 'Ha Noi', 0, '132414121244', 3, 'Trưởng phòng', 10, 15000000, 1, '2020-10-23', '2021-08-27 09:58:05', '2021-08-27 09:58:05');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `project`
+--
+
+CREATE TABLE `project` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `start_date` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `project`
+--
+
+INSERT INTO `project` (`id`, `name`, `start_date`, `created_at`, `updated_at`) VALUES
+(1, 'Moon Tower', '2020-06-04', '2021-08-27 09:54:44', '2021-08-27 09:54:44'),
+(2, 'Lily Villa', '2019-05-02', '2021-08-27 09:55:05', '2021-08-27 09:55:05');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `timekeeping`
+--
+
+CREATE TABLE `timekeeping` (
+  `id` bigint(20) NOT NULL,
+  `date` date NOT NULL,
+  `violation` int(3) DEFAULT NULL,
+  `employee_id` bigint(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `timekeeping`
+--
+
+INSERT INTO `timekeeping` (`id`, `date`, `violation`, `employee_id`, `created_at`, `updated_at`) VALUES
+(1, '2021-08-01', 0, 1, '2021-08-27 09:58:36', '2021-08-27 09:58:36'),
+(2, '2021-08-02', 0, 1, '2021-08-27 09:58:47', '2021-08-27 09:58:47'),
+(3, '2021-08-03', 0, 1, '2021-08-27 09:58:55', '2021-08-27 09:58:55');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '$2a$12$TtrZGPVfwVbpBjYJDqiCM.3P.ej3JDgHEctrf0w/Z1OF3hiYL7.hC', 'Thien dep trai', '2021-04-23 08:14:27', '2021-04-23 08:14:27');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phonenumber` (`phonenumber`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Chỉ mục cho bảng `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `timekeeping`
+--
+ALTER TABLE `timekeeping`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_id` (`employee_id`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `timekeeping`
+--
+ALTER TABLE `timekeeping`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `timekeeping`
+--
+ALTER TABLE `timekeeping`
+  ADD CONSTRAINT `employee_idd` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
